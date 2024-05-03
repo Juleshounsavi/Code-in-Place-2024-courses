42124a04a68ba89b8060f5d74c9383b5d791b55d
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-05-03 15:31:25 +0100

    Add files via upload

---
+++ Week 3 Hello name.py
@@ -0,0 +1,12 @@
+def main():
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+    name=str(input("What is your name?"))
+    print("Hello {}".format(name))
+
+if __name__ == '__main__':
+    main()
+
+
