430633abf667f4d6ec4045bf04cf21e255e669db
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-05-10 15:07:37 +0100

    Add files via upload

---
+++ Week 3 My own code Day time indicator.py
@@ -0,0 +1,17 @@
+def main():
+    print("Day time indicator")
+    h=int(input("What's the time:"))
+    if h>=0 :
+        if h>=0 and h<12:
+            print("This is the morning")
+        elif h==12:
+            print("This is the noon") 
+        elif h>12 and h<=18:
+            print("This is the afternoon") 
+        elif h>18 and h<=23:
+            print("This is the night")  
+    else:
+        print("Error: the hour must be between 0 and 23.")    
+
+if __name__ == "__main__":
+    main()
