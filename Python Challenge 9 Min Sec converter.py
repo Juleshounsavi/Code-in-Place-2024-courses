75fb54d638bc758cc07d575aa0f2d92222f9fd28
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-05-25 23:13:56 +0100

    Delete Python Challenge 9 Min Sec converter.py

--- Python Challenge 9 Min Sec converter.py
+++
@@ -1,7 +0,0 @@
-#A function that convert minutes to seconds.
-def Convert_min_sec(min):
-    sec = min*60
-    print("{} minutes is equal to {} seconds.".format(min, sec))
-
-
-Convert_min_sec(5)    
